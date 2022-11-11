be0b0e3e74b75b78158ac9ce62e673659767e8fa
Karina <karinadesyatnik27@gmail.com>
2022-11-11 23:45:32 +0300

    home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function(){
 
+    $('.menu-btn').click(function(){
+        $(this).toggleClass('active');
+        $('.menu').slideToggle();
+    });
 
+    $(".see-projects-wrapper").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+
+    });
+   
+    $('.portfolio__item').click(function(){
+        $(this).toggleClass('active');
+        $('.portfolio__item').not(this).removeClass('active');
+    })
+
+    
 
 });
