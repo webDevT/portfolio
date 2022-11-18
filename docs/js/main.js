8b67c6902f97b5b4022a042479ddbcf962990847
Karina <karinadesyatnik27@gmail.com>
2022-11-18 13:14:19 +0300

    unimation

--- docs/js/main.js
+++ docs/js/main.js
@@ -35,3 +35,23 @@ $(function(){
     
 
 });
+
+
+// gsap.config({ trialWarn: false });
+// console.clear();
+// gsap.registerPlugin(ScrollTrigger, SplitText);
+// const split = new SplitText("p", { type: "lines" });
+
+// split.lines.forEach((target) => {
+//   gsap.to(target, {
+//     backgroundPositionX: 0,
+//     ease: "none",
+//     scrollTrigger: {
+//       trigger: target,
+//       markers: true,
+//       scrub: 1,
+//       start: "top center",
+//       end: "bottom center"
+//     }
+//   });
+// });
